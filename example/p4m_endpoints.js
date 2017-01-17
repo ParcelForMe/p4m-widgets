eb6a7e95815195dff9223f333ec19c73845bbc68
krisrandall <kris.randall@gmail.com>
2017-01-17 12:12:55 +1000

    implement the checkout endpoint that does OIDC to GFS (Global Freight Solutions)

--- example/p4m_endpoints.js
+++ example/p4m_endpoints.js
@@ -78,57 +78,64 @@ exports.checkout = function(req, res) {
 
 	var cookies = new Cookies( req, res );
 
-	if ( (cookies.get('gfsCheckoutToken')==null) || (cookies.get('gfsCheckoutToken')=='') ) 
+	if (true)
+	//if ( (cookies.get('gfsCheckoutToken')==null) || (cookies.get('gfsCheckoutToken')=='') ) 
 	{
 	    
+		var url = "https://identity.justshoutgfs.com/connect/token";
+
+		var data = {
+			grant_type	: "client_credentials",
+			scope 		: "read checkout-api"
+		}
+
+		var options = {
+			url: url,
+			headers: {
+				"Authorization": "Basic " + new Buffer("parcel_4_me:needmoreparcels").toString('base64'),
+				"Content-Type": "application/x-www-form-urlencoded"
+			},
+			form : data
+		}
+
+
+		request.post(options, function(error, response, body) {
+
+			var now = new Date();
+			var cookieConf = { path : '/', expires: new Date(now.setFullYear(now.getFullYear() + 1)) , httpOnly: false };
+
+console.log('HERE with ', body);
+
+			var base64Token = new Buffer(JSON.parse(body).access_token).toString('base64');
+			cookies.set('gfsCheckoutToken', base64Token, cookieConf);
+
+			returnTemplateFile('checkout.html', '[gfs-access-token]', base64Token, res);
+			
+		});
+
+	} else {
+	
+		console.log('THIS NEVER HAPPENS CURRENTLY.. but if i stored the gfs-access-token somewhere then I could do this logic ..');
+		returnTemplateFile('checkout.html', '[gfs-access-token]', base64Token, res);
+
 	}
 
-	returnTemplateFile('checkout.html', res);
-
-
-/*
-	var url = "https://dev.parcelfor.me:44333/connect/token";
-
-	var data = {
-		grant_type		: "authorization_code",
-		code			: req.query.code,
-		redirect_uri	: "http://localhost:8081/p4m/getP4MAccessToken",
-		client_id		: "10006"
-	}
-
-	var options = {
-		url: url,
-		headers: {
-			"Authorization": "Basic " + new Buffer("10006:secret").toString('base64'),
-			"Content-Type": "application/x-www-form-urlencoded"
-		},
-		form : data
-	}
-
-
-	request.post(options, function(error, response, body) {
-
-		var now = new Date();
-		var cookieConf = { path : '/', expires: new Date(now.setFullYear(now.getFullYear() + 1)) , httpOnly: false };
-
-		cookies.set('p4mToken', JSON.parse(body).access_token, cookieConf);
-
-		res.status(200).send('<script>window.close();</script>');
-		res.end(); // needed for cookies to save !
-	});
-*/
 
 }
 
-function returnTemplateFile(file, res) {
+function returnTemplateFile(file, find, replace, res) {
 
 	fs.readFile('static_api/templates/'+file, function read(err, file_contents) {
 		if (err) {
 			res.status(500).send(err);
 		}
 
+		// IMPLEMENTING ONLY THE MOST BASIC FIND REPLACE, OF ONE SHORT CODE AND ONLY ONE OCCURANCE
+		file_contents = file_contents.toString('utf8').replace(find, replace);  
+
 		res.set('Content-Type', 'text/html');
 		res.status(200).send(file_contents);
+		res.end(); // needed for cookies to save !
 
 	});
 
